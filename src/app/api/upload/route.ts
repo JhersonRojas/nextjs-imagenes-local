4164f106f432e3c01bc8d682f5163ad20c67de4c
JhersonRojas <jhersonrojas.dev@gmail.com>
2023-11-27 11:02:26 -0500

    almacenando la imagen localmente y actuando según la respuesta del servidor

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,8 +1,40 @@
-import { type NextRequest, NextResponse } from "next/server";
+// Paquetes de Node.js a sistema
+import { writeFile } from "fs/promises";
+import { join } from "path";
+import { cwd } from "process";
+
+// Paquetes de Next y React
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function POST(request: NextRequest) {
+    try {
+        const body = await request.formData();
+        const imagen = body.get("imagen");
 
-    const body = await request.formData();
+        if (!imagen)
+            return NextResponse.json({ msg: "Debe llegar una imagen desde el cliente para procesar" }, { status: 400 });
 
-    return NextResponse.json({ msg: "Se ha ingresado la imagen" });
+        if (!(imagen instanceof Blob))
+            throw new Error("No fue posible procesar la imagen");
+
+        const bytes: ArrayBuffer = await imagen.arrayBuffer();
+        const buffer: Buffer = Buffer.from(bytes);
+
+        const path_img: string = join(cwd(), "public", "assets", imagen.name);
+
+        const uploaded = await writeFile(path_img, buffer).then(() => true).catch(() => false);
+
+        if (!uploaded)
+            throw new Error("No fue posible almacenar la imagen");
+
+        return NextResponse.json({ msg: "Se ha ingresado la imagen" }, { status: 200 });
+
+    } catch (error) {
+        console.error({
+            error: "Error al ingresar una imagen",
+            details: error,
+        });
+        return NextResponse.json({ msg: "Error al ingresar una imagen" }, { status: 500 });
+
+    }
 }
--- src/components/FormImagen.tsx
+++ src/components/FormImagen.tsx
@@ -7,6 +7,7 @@ import { useState } from "react";
 export default function FormImagen() {
 
   const [imagen, setImagen] = useState<File | undefined>(undefined);
+  const [respuesta, setRespuesta] = useState<string | null>(null);
 
   const handleChange = (event: any) => {
     event.preventDefault();
@@ -14,21 +15,58 @@ export default function FormImagen() {
     setImagen(files[0]);
   };
 
+  const handleUpload = async (event: any) => {
+    try {
+      event.preventDefault();
+      if (!imagen) throw new Error("Es necesaria una imagen para guardar");
+
+      const formdata = new FormData();
+      formdata.append("imagen", imagen);
+
+      const response = await fetch("http://localhost:3000/api/upload", {
+        method: "POST",
+        body: formdata
+      });
+
+      const data = await response.json();
+      setRespuesta(data.msg)
+
+    } catch (error) {
+      console.error({
+        error: "Error al ingresar la imagen",
+        details: error
+      });
+      setRespuesta("A ocurrido un error al ingresar la imagen");
+
+    } finally {
+      setTimeout(() => {
+        setRespuesta(null)
+      }, 3000);
+    }
+  };
+
   return (
     <>
       <form id="form-imagen" className="w-full h-[15%] p-6 grid grid-cols-3 items-center">
         <input type="file" name="image" id="imagen" accept=".jpg, .jpeg, .png. svg, .webp" onChange={handleChange} className="col-span-2" />
-        <button type="submit" className="bg-zinc-900 text-white text-sm px-4 py-2 rounded-md col-span-1">
-          Guardar
+        <button
+          disabled={imagen ? false : true}
+          onClick={handleUpload}
+          className="bg-zinc-900 hover:bg-zinc-950 disabled:bg-zinc-700 text-white text-sm px-4 py-2 rounded-md col-span-1 transition-all"
+        >
+          Ingresar
         </button>
       </form>
-      <figure className="w-full h-[70%] p-6 overflow-auto">
+      <figure className="w-full h-[70%] p-6 overflow-auto relative">
         {
           imagen
             // eslint-disable-next-line @next/next/no-img-element
             ? <img src={URL.createObjectURL(imagen)} alt="Imagen a ingresar" />
             : <div className="bg-zinc-900 text-white w-full h-full rounded-md grid justify-center items-center"> Sin imagen </div>
         }
+        <span className={(!respuesta && "hidden") + " " + "bg-white text-black text-xs p-4 absolute bottom-5 right-5 border-zinc-950 border-[1px] rounded-md transition-all"}>
+          {respuesta && respuesta}
+        </span>
       </figure>
     </>
   );
